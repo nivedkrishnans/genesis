5dee032dafe33ee7260abc04c175771fdaf61ed3
nived <nived.krishnans@yahoo.com>
2018-05-11 00:40:36 +0530

    Removed from exc in manage.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 def home(request):
     return render(request, 'home/home.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	#url(r'^$', include('home.urls')),
 	url(r'^originals/', include('originals.urls')),
+	url(r'', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
