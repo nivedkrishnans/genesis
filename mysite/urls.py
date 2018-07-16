525eb6834d2be445a17c95dfed199f0ee22b03b1
nived <nived.krishnans@yahoo.com>
2018-07-16 18:28:07 +0530

    Improved the links to Pravega 2017&18
    Go to /2017 or /2018 to visite previous pravega pages.

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^originals/', include('originals.urls')),
+	url(r'^2018/$', views.pravega2018 , name="pravega2018"),
+	url(r'^2017/$', views.pravega2017 , name="pravega2017"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ mysite/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import redirect
+def pravega2018(request):
+    return redirect(to="/static/Previous_Pravega/2018/index.html")
+
+def pravega2017(request):
+    return redirect(to="/static/Previous_Pravega/2017/index.html")
