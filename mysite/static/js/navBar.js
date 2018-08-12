52b50e9eaebfef42f6fcd419d1d225e34f5565e0
nived <nived.krishnans@yahoo.com>
2018-08-12 21:16:22 +0530

    Made navBar and navBarAutoHide inline in html.

--- mysite/static/js/navBar.js
+++ mysite/static/js/navBar.js
@@ -1,37 +1,9 @@
 var navBar = document.getElementById("navBar");
 var navMobile = document.getElementById('navBarMobile');
-var navHeight = navBar.offsetHeight;
 var homeNav = document.getElementById("homeNav");
-var hoverNavDisplay = document.getElementById("hoverNavDisplay");
 var ham = document.getElementById('hamburgerButton');
 var navDisplay = false;     //whether or not nav menu is displayed on mobile
 var navSets = document.getElementsByClassName("navLinkSet");
-
-/*
-function navAppear(){   // changes display of navBar based on scroll position
-
-    if(window.pageYOffset > 10){
-        navMobile.style.top = "0";
-            if(document.documentElement.clientWidth >= 760)
-                navBar.style.top = "0";
-    }
-    else{
-        navMobile.style.top = "-50px";
-        toggleNavMobile(false);
-        if(document.documentElement.clientWidth< 760)
-            navBar.style.top = "-400px";
-        else
-            navBar.style.top = "-100px";
-    }
-}
-
-
-window.onscroll = function(){
-    navAppear();
-
-};
-
-*/
 
 window.onresize = function(){
     toggleNavMobile(false);
@@ -40,16 +12,13 @@ window.onresize = function(){
         navSets[0].style.flex = "3 0 0";
         navSets[1].style.flex = "1 0 0";
         navSets[2].style.flex = "4 0 0";
-
     }
     else{
         navSets[0].style.flex =  "1 0 100%";
         navSets[1].style.flex =  "1 0 100%";
         navSets[2].style.flex =  "1 0 100%";
     }
-    //navAppear();
 };
-
 
 window.onload = function(){
     toggleNavMobile(false);
@@ -64,15 +33,13 @@ window.onload = function(){
         navSets[1].style.flex =  "1 0 100%";
         navSets[2].style.flex =  "1 0 100%";
     }
-    //navAppear();
 };
-
 
 function toggleNavMobile(disp){         //shows nav menu for mobile if argument is true, otherwise hides
 
     if(!disp){
         navMobile.style.borderBottom = "#ffee9c solid 1px";
-        navBar.style.top = "-400px";
+        navBar.style.top = "-280px";
         ham.classList.remove('is-active');
         homeNav.style.opacity= "0";
     }
@@ -85,23 +52,6 @@ function toggleNavMobile(disp){         //shows nav menu for mobile if argument 
     navDisplay = !navDisplay;
 }
 
-
 ham.addEventListener("click", function() {
     toggleNavMobile(!navDisplay);
     }, true);
-
-/*
-hoverNavDisplay.onmouseover = function(){
-    if(document.documentElement.clientWidth >= 760){
-            navBar.style.top = "0";
-    }
-
-}
-
-
-hoverNavDisplay.onmouseout = function(){
-    if((document.documentElement.clientWidth >= 760) && (window.pageYOffset < 10)){
-            navBar.style.top = "-100px";
-    }
-}
-*/
