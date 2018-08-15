1655d60fa4e3aafeb8bdf39c30fe61b9d52e442b
nived <nived.krishnans@yahoo.com>
2018-08-15 16:27:19 +0530

    Slides on homepage. Quicklinks added. Home scroll bug fixed.
    Checking out git ignore settings

--- essentials/static/js/sectionsDisplay.js
+++ essentials/static/js/sectionsDisplay.js
@@ -4,30 +4,31 @@ document.body.onload = function(){
 
 var n = 2;
 
-if (document.documentElement.width >= 760) 
+if (document.documentElement.width >= 760)
     n=3;
 
 var secns = document.getElementsByClassName("home-section");
+var locTmp,pos;
 
-window.addEventListener('scroll', function (){
-    var secns = document.getElementsByClassName("home-section");
-    
+function sectionDisplay(){
     for(i=1;i<secns.length;i++){
-        locTmp = secns[i].getBoundingClientRect();
-        if(n*locTmp.top < document.documentElement.clientHeight){
+         locTmp= secns[i].getBoundingClientRect();
+         pos=n;
+        if(i==(secns.length-1))
+            pos=1;
+        if(pos*locTmp.top < document.documentElement.clientHeight){
             secns[i].style.webkitFilter = "brightness(100%)";
             secns[i].style.webkitTransform = "scale(1)";
             secns[i].style.filter = "brightness(100%)";
             secns[i].style.transform = "scale(1)";
+            if(i==(secns.length-1))
+                 window.removeEventListener('scroll', function(){sectionDisplay();}, false);
         }
-        if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
-            secns[i].style.webkitFilter = "brightness(100%)";
-            secns[i].style.webkitTransform = "scale(1)";
-            secns[i].style.filter = "brightness(100%)";
-            secns[i].style.transform = "scale(1)";
-        }
+        pos=n;
     }
-}, false);
+}
+
+window.addEventListener('scroll', function(){sectionDisplay();}, false);
 
 function activateAnims(){
     document.querySelector('#title').style.animationPlayState = 'running';
