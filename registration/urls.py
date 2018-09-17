022628707d765b51a4885fe725c308792e5bb13f
nived <nived.krishnans@yahoo.com>
2018-09-17 20:21:53 +0530

    Tring to fix django.contrib.auth.views' has no attribute 'password_reset' v2

--- registration/urls.py
+++ registration/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
 	url(r'^activate/account/$', views.activateAccount),
 	url(r'^$', views.registration_index, name="registration"),
 	url('^password_reset/$', django.contrib.auth.views.PasswordResetView.as_view(
-		'email_template_name': 'registration/email_templates/password_reset.txt',
-		'html_email_template_name': 'registration/email_templates/reset_password_email.html',
-		'subject_template_name': 'registration/email_templates/password_reset_subject.txt'
+		'email_template_name'='registration/email_templates/password_reset.txt',
+		'html_email_template_name'='registration/email_templates/reset_password_email.html',
+		'subject_template_name'='registration/email_templates/password_reset_subject.txt'
 	),name='password_reset'),
 	url(r'^', include('django.contrib.auth.urls')),
 ]
